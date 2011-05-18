20daa5192fe1c2b6550b5d567520f926fea50613
Glenn Bech <glenn@glennux.(none)>
2011-05-18 21:35:02 +0200

    first commit

---
+++ src/com/glennbech/prefermentz/Calculator.java
@@ -0,0 +1,20 @@
+package com.glennbech.prefermentz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ *
+ *
+ */
+public class Calculator extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
