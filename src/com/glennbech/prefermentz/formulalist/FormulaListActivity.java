ba0496f0b4d957ca40a537fd21eec8afb1c8c2af
unknown <glennn@.(none)>
2011-06-30 22:36:54 +0200

    Dialog Finished.

--- src/com/glennbech/prefermentz/formulalist/FormulaListActivity.java
+++ src/com/glennbech/prefermentz/formulalist/FormulaListActivity.java
@@ -33,6 +33,11 @@ import java.util.List;
 public class FormulaListActivity extends OrmLiteBaseActivity<RecipeOpenHelper> {
 
 
+    private RecipeOpenHelper roHelper;
+    private FormulaListAdapter formulaListAdapter;
+    List<Formula> formulas;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,16 +45,52 @@ public class FormulaListActivity extends OrmLiteBaseActivity<RecipeOpenHelper> {
         ListView lv = (ListView) findViewById(R.id.formulaList);
         registerForContextMenu(lv);
 
+        roHelper = new RecipeOpenHelper(this);
+
         View v = findViewById(R.id.newformulabutton);
         v.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Context c = FormulaListActivity.this;
-                AlertDialog.Builder alert = new AlertDialog.Builder(c);
-                alert.setTitle("Jalls");
+                final EditText recipeName = new EditText(c);
+                final AlertDialog alert = new AlertDialog.Builder(FormulaListActivity.this).create();
+                alert.setTitle("New Formula name");
                 LinearLayout linearLayout = new LinearLayout(FormulaListActivity.this);
-                linearLayout.addView(new Button(c));
-                 alert.show();
-                //startActivityForResult(new Intent().setClass(FormulaListActivity.this, NewFormulaActivity.class), 1);
+                linearLayout.setOrientation(LinearLayout.VERTICAL);
+                linearLayout.addView(recipeName);
+
+                LinearLayout buttons = new LinearLayout(FormulaListActivity.this);
+                buttons.setOrientation(LinearLayout.VERTICAL);
+
+                Button okButton = new Button(c);
+                okButton.setText("Ok");
+                buttons.addView(okButton);
+                Button cancelButton = new Button(c);
+                cancelButton.setText("Cancel");
+                buttons.addView(cancelButton);
+                linearLayout.addView(buttons);
+                alert.setView(linearLayout);
+                alert.show();
+
+                okButton.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View view) {
+                        Dao<Formula, String> dao = roHelper.getFormalaDao(FormulaListActivity.this);
+                        Formula f = new Formula(recipeName.getText().toString().toLowerCase());
+                        try {
+                            dao.create(f);
+                            Intent intent = new Intent().setClass(FormulaListActivity.this, RecipeActivity.class);
+                            SharedPreferences p = PreferenceManager.getDefaultSharedPreferences(FormulaListActivity.this);
+                            float defaultWeight = p.getFloat("defaultWeight", 1000f);
+                            Recipe r = new Recipe(f, defaultWeight);
+                            intent.putExtra(Recipe.class.getName(), (Serializable) r);
+                            startActivity(intent);
+                        } catch (SQLException e) {
+                            throw new RuntimeException(e);
+                        } finally {
+                            alert.cancel();
+                        }
+                    }
+                });
+
             }
         });
 
@@ -58,39 +99,10 @@ public class FormulaListActivity extends OrmLiteBaseActivity<RecipeOpenHelper> {
     }
 
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (resultCode == RESULT_OK) {
-            ListView lv = (ListView) findViewById(R.id.formulaList);
-            ((ArrayAdapter) lv.getAdapter()).notifyDataSetChanged();
-        }
-    }
-
-    @Override
     protected void onResume() {
         super.onResume();
-        List<Formula> formulas;
-        try {
-            Dao dao = getHelper().getDao(Formula.class);
-            formulas = dao.queryForAll();
-            for (Formula f : formulas) {
-                f.loadFormulaComponents(this);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        Collections.sort(formulas, new Comparator<Formula>() {
-            public int compare(Formula o1, Formula o2) {
-                return o1.getName().compareTo(o2.getName());
-            }
-        });
-        ListView lv = (ListView) findViewById(R.id.formulaList);
-        lv.setAdapter(new FormulaListAdapter(this, formulas));
-
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        refreshList();
+        ((ListView) findViewById(R.id.formulaList)).setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Formula f = (Formula) adapterView.getItemAtPosition(i);
                 Intent intent = new Intent().setClass(FormulaListActivity.this, RecipeActivity.class);
@@ -101,6 +113,27 @@ public class FormulaListActivity extends OrmLiteBaseActivity<RecipeOpenHelper> {
                 startActivity(intent);
             }
         });
+    }
+
+    private void refreshList() {
+        try {
+            Dao dao = getHelper().getDao(Formula.class);
+            formulas = dao.queryForAll();
+            for (Formula f : formulas) {
+                f.loadFormulaComponents(this);
+            }
+            Collections.sort(formulas, new Comparator<Formula>() {
+                public int compare(Formula o1, Formula o2) {
+                    return o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase());
+                }
+            });
+            ListView lv = (ListView) findViewById(R.id.formulaList);
+            formulaListAdapter = new FormulaListAdapter(this, formulas);
+            lv.setAdapter(formulaListAdapter);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
